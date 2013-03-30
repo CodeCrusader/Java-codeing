7f246eb926753a2db12dc69ababcee937aba3a67
Justin Vitale <Quadcorenerd@gmail.com>
2013-03-29 17:37:09 -0700

    Done!

--- Crynix rolling game/src/Main.java
+++ Crynix rolling game/src/Main.java
@@ -6,26 +6,38 @@ public static void main(String[] args){
 		answer = 1+Number.nextInt(10);
 		int remain = 0;
 		int deathcont = 0;
+		int min = 5;
 	
 
 	System.out.println("This is a guessing game where you have to guess a number between 1-10 you have 6 attempts before you fail, let the games begin!");
 	System.out.println("A number has been picked");
+	System.out.println("Made for Zach");
 	Scanner number = new Scanner(System.in);
 	while(remain < 6){
-	System.out.println("Enter your guess 1-10 : " + answer);
+	System.out.println("Enter your guess 1-10 : ");
 	int dimmer = number.nextInt();
-	if(dimmer == 2003)System.out.println("FUCK YOU THERE IS NO CHEATING IN THIS FUCKING GAME!");
-	if(dimmer == answer) remain = 6;
+	if(dimmer == answer) remain = 6; else{
 	System.out.println("Guess again");
+	System.out.println("You have " + min + " Guess's left");
+	remain++;
+	deathcont++;
+	min--;
+	}
 	}
 	if(deathcont==6)System.out.println("You have failed the game"); else{
 	System.out.println("YOUR WON! THE ANSWER WAS " + answer);
-	remain++;
-	deathcont++;
+	
 	}
+
 }	
 }
-
+// goal is ==
+// [3/28/13 8:44:25 PM] Zach Latta: I like games.
+// [3/28/13 8:44:40 PM] Zach Latta: I want you to make a game for me where I need to guess a number between 1 and 10.
+// [3/28/13 8:44:53 PM] Zach Latta: So a guessing game.
+// .
+// . 
+// .
  // Links used to help build this
 //http://www.youtube.com/watch?v=fFg40hzi9X0
 //http://www.youtube.com/watch?v=AhwIYAXPASw
