6eded89fc9498186d73795b289362df164134cd7
Justin Vitale <Quadcorenerd@gmail.com>
2013-04-01 19:59:18 -0700

    Dota

---
+++ Gui message dialog/src/Main.java
@@ -0,0 +1,11 @@
+import javax.*;
+import javax.swing.JOptionPane;
+public class Main {
+public static void main(String[] args){
+	int a = 3;
+	while(a < 6){
+	JOptionPane.showMessageDialog(null,"Hello this is a simple textbox");
+	}
+	
+}
+}
